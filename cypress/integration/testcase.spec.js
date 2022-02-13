9d434341726b4d602ec6e3ae468eb129f225c242
bcetin6 <99258764+bcetin6@users.noreply.github.com>
2022-02-13 22:16:21 +0300

    test cases for buttons under the search bar + chrome security added

--- cypress/integration/testcase.spec.js
+++ cypress/integration/testcase.spec.js
@@ -4,7 +4,7 @@
 
 
     it('Visit the URL', () => {
-        cy.visit('https://www.google.com/');
+        cy.visit('https://www.google.com.tr/');
     });
 
 describe('Checking UI', ()=>{
@@ -27,13 +27,27 @@ describe('Checking UI', ()=>{
             cy.go('back');    
         })
 
+
     })
 
     describe('Checking the buttons below the search bar', () =>{
-        it('Check the button "google search"', () =>{
-            cy.get('.gNO89b').should('be.visible').contains("Google'da Ara").click();
+        it.only('Check the button googleda ara', () =>{
+            cy.visit('https://www.google.com.tr/'); //bunu sonradan sil
+            cy.get('.FPdoLc > center > .gNO89b').should('be.visible').contains("Google'da Ara");
+            cy.get('.gLFyf').click().type('huawei');
+            cy.get('.FPdoLc > center > .gNO89b').click();
+            cy.go('back'); 
+
             
         })
+        it('Check the button kendimi sansli hissediyorum', () => {
+            cy.visit('https://www.google.com.tr/'); //bunu sonradan sil
+            cy.get('.FPdoLc > center > .RNmpXc').contains('Kendimi Şanslı Hissediyorum').click();
+            cy.go('back');
+        
+
+        })
+        
     })
 
 }
